7ac1f92e397e54932cd17629090b8f4063e14e6a
bim-g <ibmussafb@gmail.com>
2021-03-02 13:24:22 +0200

    change wepesi configuration

--- api/config/init.php
+++ api/config/init.php
@@ -15,7 +15,7 @@
             "token_name"=>"token"
         )
     );
-    
+
     function getSubDirectories($dir)    {
         $subDir = array();
         $directories = array_filter(glob($dir), 'is_dir');
@@ -23,14 +23,24 @@
         foreach ($directories as $directory) $subDir = array_merge($subDir, getSubDirectories($directory . '/*'));
         return $subDir;
     }
+
     // will load all class from the class folder
     spl_autoload_register(function($class){
         $dirs = getSubDirectories("class");
+        $class_arr=explode("\\",$class);
+        $len=count($class_arr);
+        $classFile=$class_arr[($len-1)];
         foreach($dirs as $dir){
-            $file=$dir."/".$class.".php";
+            $file=$dir."/". checkFileExtension($classFile);
             if (is_file($file)) { // check if the file exist
                 require_once($file); // incluse the file request if it exist
             }
         }
     });
+
+    function checkFileExtension($fileName){
+        $file_parts = pathinfo($fileName);
+        $file = (isset($file_parts['extension']) && $file_parts['extension'] == "php") ? $fileName : $fileName . ".php";
+        return $file;
+    }
     require_once 'controller/function.php';

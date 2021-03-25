b07247e2688c46830316f97a71f9524428da67fe
bim-g <ibmussafb@gmail.com>
2021-03-25 23:38:41 +0200

    add cors module

--- api/index.php
+++ api/index.php
@@ -1,6 +1,7 @@
 <?php
-    header("Access-Control-Allow-Origin: *");
-    // header("Content-Type: application/json; charset=UTF-8");
+    require_once('config/cors.php');
     require_once 'config/init.php';
+    // manage cors for API Request
+    cors();
     require_once 'route/index.php';
 ?>
